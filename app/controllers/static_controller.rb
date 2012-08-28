56f8949951b3778ffbf3c6776f71484fcbae9037
Chris Continanza <christopher.continanza@gmail.com>
2012-08-27 23:02:53 -0700

    don't hardcode the url

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,10 @@
 class StaticController < ApplicationController
-  
+
   def index
     @commands = Command.includes(:user).all
     @users = User.all
   end
-  
+
   def engine
     files = [
       "javascript:",
@@ -12,7 +12,8 @@ class StaticController < ApplicationController
       File.open(Rails.root + "public/zepto.min.js").read,
       File.open(Rails.root + "public/caboose.js").read,
     ]
-    render :js => files.join(";\n\n")
+    url = "#{request.protocol}#{request.host_with_port}/exec/%s?v=1.0"
+    render :js => files.join(";\n\n").gsub(':queriac_url',url)
   end
-  
+
 end
--- public/caboose.js
+++ public/caboose.js
@@ -2,7 +2,7 @@ Zepto(
   function($) {
     _s=document.createElement('script');
     _s.type='text/javascript';
-    _s.src='https://queriac2.herokuapp.com/exec/%s?v=1.0';
+    _s.src=':queriac_url';
     document.getElementsByTagName('head')[0].appendChild(_s);
     void(0);
   }
